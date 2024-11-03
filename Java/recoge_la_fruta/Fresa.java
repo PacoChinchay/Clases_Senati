79f51f886d149a4ed57106bc8cac223157bdbfa5
PacoChinchay <pacochinchay350@gmail.com>
2024-11-03 17:29:26 -0500

    Eliminado repositorio interno de Java/AhorroPeru

--- Java/recoge_la_fruta/Fresa.java
+++
@@ -1,9 +0,0 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-public class Fresa extends Actor
-{
-    public void act()
-    {
-        setLocation(getX(), getY() +2); 
-    }
-}
